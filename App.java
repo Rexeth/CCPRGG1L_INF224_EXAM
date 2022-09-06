06b1f7034e9452cd7a47391a6af3e8c09a74c358
Rexeth <106716657+Rexeth@users.noreply.github.com>
2022-09-06 10:00:33 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args);
+
+    //character class
+    Student Rex = new Student[];
+
+    Rex.surname = "Macalalad";
+    Rex.firstnmame = "Rex";
+    Rex.middleInitial = 'H';
+    Rex.dateofbirth = "08/02/2001";
+    Rex.studentnumber = "2020-300004";
+    Rex.emailaddress = "macalaladrh@students.national-u.edu.ph";
+    Rex.IamAwesome = true;
+    Rex.sayMystudentNumber();
+    Rex.sayMyemailaddress();
+    Rex.sayMystudentIamAwesome();
+
+
+
+
+         
+
+    
+}
---
+++ Student.java
@@ -0,0 +1,34 @@
+public class Student {
+    // character attributes
+    String surname;
+    String firstname;
+    char middleInitial;
+    String dateofbirth;
+    int studentnumber;
+    String emailaddress;
+    Boolean IamAwesome;
+
+    //Character method
+    public void sayMysurname() {
+        System.out.printIn("My surname is " + surname);
+    }
+    public void sayMyfirstname() {
+        System.out.printIn("My firstname is " + firstname);
+    
+}
+     public void middleInitial() {
+        System.out.printIn("My middleInitial is" + middleInitial);
+    }
+    public void sayMydateofbirth() {
+        System.out.printIn("My  Birthday is " + dateofbirth);
+    }
+    public void sayMystudentNumber() {
+        System.out.printIn("My studentnumber is" + studentnumber);
+    }
+    public void sayMysurname() {
+        System.out.printIn("My emailaddress" + emailaddress);
+    }
+    
+    public void sayMystudentIamAwesome() {
+        System.out.printIn("I Am Awesome " + IamAwesome);
+    }
